2d4bd3b0b7ad7b83abbecdcd9fdcfc158ad954da
sullivan_lb <sullivan.lb56@gmail.com>
2021-01-15 21:26:15 +0100

    routes créé

--- src/Site_Web/rosetta_site_web/src/App.js
+++ src/Site_Web/rosetta_site_web/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Accueil from './pages/Accueil';
+import Devis from './pages/Devis';
+import Client from './pages/Client';
+import Scenario from './pages/Scenario';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+         <Route exact path="/" component={Accueil}/>
+         <Route exact path="/client" component={Client}/>
+         <Route exact path="/devis" component={Devis}/>
+         <Route exact path="/scenario" component={Scenario}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
