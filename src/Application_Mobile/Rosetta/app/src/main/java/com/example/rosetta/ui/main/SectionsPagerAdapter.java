07592a12813db99a311e9e83401cbee0161e4c25
root <root@Gellert.localdomain>
2021-01-15 22:16:54 +0100

    interface principale réussie

--- src/Application_Mobile/Rosetta/app/src/main/java/com/example/rosetta/ui/main/SectionsPagerAdapter.java
+++ src/Application_Mobile/Rosetta/app/src/main/java/com/example/rosetta/ui/main/SectionsPagerAdapter.java
@@ -17,7 +17,7 @@ import com.example.rosetta.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -27,9 +27,20 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new ClientFragment();
+                break;
+            case 1:
+                fragment = new ScenarioFragment();
+                break;
+            case 2:
+                fragment = new DevisFragment();
+                break;
+        }
+
+        return fragment;
     }
 
     @Nullable
@@ -40,7 +51,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 2;
+        // Show 3 total pages.
+        return 3;
     }
 }
