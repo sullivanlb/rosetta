d51d6fc9b8e89630703f48cd9f54a415035a94ed
root <root@DESKTOP-OT70A9M.localdomain>
2021-01-26 23:26:36 +0100

    Avencement de la page devis > barre de recherche

--- src/Site_Web/rosetta_site_web/src/pages/Devis.js
+++ src/Site_Web/rosetta_site_web/src/pages/Devis.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Row, Col, Container, Form, Button } from 'react-bootstrap';
+import { MDBCol, MDBIcon} from "mdbreact";
+import "../style/Devis.css";
+
 
 const Devis = () => {
     return (
         <div>
-            Devis
+             <h3>Devis</h3>
+            <img class="ImagelogoPlombier" src="/img/logo-plombiers.png" alt="" />
+            <Container fluid>
+                <Row>
+                    <Col className="col1-AffichageDevis" md={10}>
+                    <MDBCol md="6">
+                    <form className="form-inline mt-4 mb-4">
+                        <MDBIcon icon="search" />
+                        <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Rechercher" aria-label="Rechercher" />
+                    </form>
+                    </MDBCol>
+
+                   
+
+                    </Col>
+                    <Col className="col2-ButtonDevis" md={2}>
+
+                    <button type="button" class="btn btn-light">Ajouter Devis</button>
+                    <button type="button" class="btn btn-light">Modifier Devis</button>
+                    <button type="button" class="btn btn-light">Supprimer Devis</button>
+
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
