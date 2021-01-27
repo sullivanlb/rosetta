49c9f7e55b3292a39033a40d6ad7ba511e60eed0
sullivan_lb <sullivan.lb56@gmail.com>
2021-01-28 00:01:16 +0100

    Affichage d'un client ajouté

---
+++ src/Site_Web/rosetta_site_web/src/composants/AffichageClient.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class AffichageClient extends Component {
+  render() {
+    return (
+      <div className="card card-body mb-3">
+        <h4>{this.props.nom}</h4>
+        <ul className="list-group" />
+        <li className="list-group-item">Nom : {this.props.nom}</li>
+        <li className="list-group-item">Prenom : {this.props.prenom}</li>
+        <li className="list-group-item">Email : {this.props.email}</li>
+      </div>
+    );
+  }
+}
---
+++ src/Site_Web/rosetta_site_web/src/composants/ListeClient.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+import AffichageClient from "./AffichageClient";
+
+export default class ListeClient extends Component {
+  state = {
+    contact: [
+      {
+        id: 1,
+        nom: "Jacque",
+        prenom: "Lors",
+        email: "jacquo@gmai.com",
+        tel: "0652124100",
+      },
+      {
+        id: 2,
+        nom: "Marie",
+        prenom: "Poli",
+        email: "popolli@hotmail.com",
+        tel: "0245789914",
+      },
+      {
+        id: 3,
+        nom: "Marc",
+        prenom: "Castier",
+        email: "castanier@superfree.com",
+        tel: "0155669988",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div>
+        <ListGroup>
+          <ListGroupItem href="#link1">Link 1</ListGroupItem>
+          <ListGroupItem href="#link2">Link 2</ListGroupItem>
+          <ListGroupItem href="#link3">Link 3</ListGroupItem>
+        </ListGroup>
+      </div>
+    );
+  }
+}
