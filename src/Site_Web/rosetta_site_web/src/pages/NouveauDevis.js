8f424c5fef7f3411ae4e1e7dbf94f497718acb8b
root <root@DESKTOP-OT70A9M.localdomain>
2021-01-31 13:06:59 +0100

    Page Davis > Formulaire ajouter Devis

--- src/Site_Web/rosetta_site_web/src/pages/NouveauDevis.js
+++ src/Site_Web/rosetta_site_web/src/pages/NouveauDevis.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Col, Button, Form, Container, Row, FormLabel, FormControl, FormGroup } from 'react-bootstrap';
+import { Col, Button, Form, Container, Row, FormLabel, FormControl, FormGroup, Table } from 'react-bootstrap';
+import '../style/NouveauDevis.css';
 import FormCheckInput from 'react-bootstrap/esm/FormCheckInput';
 import FormFileInput from 'react-bootstrap/esm/FormFileInput';
 
@@ -7,64 +8,159 @@ import FormFileInput from 'react-bootstrap/esm/FormFileInput';
 
 const NouveauDevis = () => {
     return(
+
         <Container className="nouveau_devis_form">
+            <Row>
+                <Col><br></br></Col>
+            </Row>
+
+            <Row>    
+            <Col md="4">
+                <img className="logo ml-4 mt-2" src="/img/logo.jpg" alt="" />
+            </Col>
+            </Row>
+
+            <Row>
+                <FormLabel> Allô Dépanne Service </FormLabel>
+            </Row>
+
+            <Row>
+                <FormLabel> Thierry Hourdier </FormLabel>
+            </Row>
+
             <Row>
                 <Col><br></br></Col>
             </Row>
             
             <Form.Group >
                 <FormLabel>Devis n° </FormLabel>
-                <FormControl placeholder="Entrer le numéro du devis"/>
+                <FormControl placeholder="exemple : 156"/>
             </Form.Group>
 
+            <Row >
+            <Col md="2">        
+                <FormLabel>Edité le </FormLabel>
+                <FormControl placeholder="31/01/2021"/>
+            </Col> 
             
-            <Form.Group>
-            <FormLabel>Edité le </FormLabel>
-            <FormControl placeholder="Entrer la date de la réalisation du devis"/>
-            </Form.Group>
+            <Col md="6"></Col>
          
-            <Form.Group>
-            <FormLabel>Client  </FormLabel>
+            <Col md="4">
             <select>
                 <option>Sélectionner un client </option>
                 <option>Alice Gontard</option>
                 <option>Christophe Garcia</option>
                 <option>Sullivan LeBoeuf</option>
             </select>
-            </Form.Group>
+            </Col>
+            </Row>
 
-            <Form.Group>
+            <Row>
+            <Col md="2">
                 <FormLabel>Début des travaux le </FormLabel>
-                <FormControl placeholder="Entrer la date où les travaux vont commencé"/>
-            </Form.Group>
+                <FormControl placeholder="25/02/2021"/>
+            </Col>
 
-            <Form.Group>
-            <FormLabel>Scénario  </FormLabel>
+            <Col md="6"></Col>
+
+            <Col md="4">
             <select>
                 <option>Sélectionner un scénario </option>
                 <option>Installation d'un chauffe-eau électrique </option>/option>
                 <option>Remplacement de la baignoire en douche</option>
             </select>
-            </Form.Group>
+            </Col>
 
-            <Form.Group>
-                <FormLabel>Durée estimée des travaux </FormLabel>
-                <FormControl placeholder="Entrer la durée estimée"/>
-            </Form.Group>
+            <Col md="4">
+            <FormLabel>Durée estimée des travaux </FormLabel>
+            <FormControl placeholder="2 semaines"/>
+            </Col>
+            </Row>
 
-            <Form.Group>
-                <FormLabel>Description : </FormLabel>
-                <FormControl placeholder="Entrer la description"/>
-            </Form.Group>
+            <Row>
+                <Col><br></br></Col>
+            </Row>
+               
+            <FormLabel>Description : </FormLabel>
+            <FormControl placeholder="Entrer la description"/>
 
-            <Button variant="ligth" type="submit">
-                Ajouter une ligne 
-            </Button>
+            <Row>
+                <Col><br></br></Col>
+            </Row>
 
-            <Button variant="ligth" type="submit">
-                Générer le devis en PDF
-            </Button>
+            <div class="alert alert-secondary" role="alert">
+                DETAIL DES PIECES
+            </div>
             
+            <Table>
+                <thead>
+                    <tr>
+                        <th> Référence </th>
+                        <th> Description </th>
+                        <th> Quantité </th>
+                        <th> Unité </th>
+                        <th> Prix </th>
+                    </tr>
+                </thead>
+            </Table>
+
+            <Row>
+            <Col md="3">
+                <FormControl placeholder="ref : 85236"/>
+            </Col>
+            <Col md="3">
+                <FormControl placeholder="Tuyau"/>
+            </Col>
+            <Col md="2">
+                <FormControl placeholder="1"/>
+            </Col>
+            <Col md="2">
+                <FormControl placeholder="2,20"/>
+            </Col>
+            <Col md="2">
+                <FormControl placeholder="2,20"/>
+            </Col>
+            </Row>
+
+            <Row>
+                <Col><br></br></Col>
+            </Row>
+
+            <div class="alert alert-secondary" role="alert">
+                TOTAL 
+            </div>
+
+            <Row>
+
+            </Row>
+            <Col md="4">
+                <FormLabel> Prix Hors Taxe </FormLabel>
+                <FormControl placeholder="100"/>
+            </Col>
+            <Col md="4">
+                <FormLabel> TVA (%) </FormLabel>
+                <FormControl placeholder="20"/>
+            </Col>
+            <Col md="4">
+                <FormLabel> Prix  </FormLabel>
+                <FormControl placeholder="120"/>
+            </Col>
+
+            <Row>
+                <Col><br></br></Col>
+            </Row>
+
+            <Row>
+            <Col md="3">
+                <Button variant="ligth" type="submit"> Ajouter une ligne </Button>
+            </Col>
+            <Col md="3">
+                <Button variant="ligth" type="submit">Enregistrée </Button>
+            </Col>
+            <Col md="3">
+                <Button variant="ligth" type="submit">Générer le devis en PDF </Button>
+            </Col>
+            </Row>
         </Container>
     )
 }
