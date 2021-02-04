b43ea9c07485c0950a9241ad1637dcfaff74b7de
sullivan_lb <sullivan.lb56@gmail.com>
2021-02-04 23:29:23 +0100

    import inutile enlevé

--- src/Site_Web/rosetta_site_web/src/composants/OpenStreetMap.js
+++ src/Site_Web/rosetta_site_web/src/composants/OpenStreetMap.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-import L from 'leaflet';
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import L from "leaflet";
 
 const position = [47.660137584787215, -2.7846692596596148];
 
-
 let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow
+  iconUrl: icon,
+  shadowUrl: iconShadow,
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
@@ -23,7 +22,7 @@ export default class OpenStreetMap extends Component {
         center={position}
         zoom={13}
         scrollWheelZoom={true}
-        style={{ width: '100%', height: '30%' }}
+        style={{ width: "100%", height: "30%" }}
       >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
